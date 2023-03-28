71e68296504e05b871311e0e6b8a53740c8e816c
mfathif <fathifarhat@student.telkomuniversity.ac.id>
2023-03-28 13:07:49 +0700

    v.1.2.1

--- proses.php
+++ proses.php
@@ -1,20 +1,43 @@
-<?php
-// ambil data dari form
-$angka1 = $_POST['angka1'];
-$angka2 = $_POST['angka2'];
-$operator = $_POST['operator'];
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kalkulator Sederhana</title>
+</head>
+<body>
+	<form method="post">
+		<input type="number" name="angka1" placeholder="Masukkan angka pertama"><br>
+		<input type="number" name="angka2" placeholder="Masukkan angka kedua"><br>
+		<select name="operator">
+			<option value="tambah">+</option>
+			<option value="kurang">-</option>
+			<option value="kali">*</option>
+			<option value="bagi">/</option>
+		</select><br>
+		<input type="submit" name="submit" value="Hitung">
+	</form>
+	<?php
+		if(isset($_POST['submit'])){
+			$angka1 = $_POST['angka1'];
+			$angka2 = $_POST['angka2'];
+			$operator = $_POST['operator'];
 
-// hitung hasil
-if ($operator == '+') {
-	$hasil = $angka1 + $angka2;
-} else if ($operator == '-') {
-	$hasil = $angka1 - $angka2;
-} else if ($operator == '*') {
-	$hasil = $angka1 * $angka2;
-} else if ($operator == '/') {
-	$hasil = $angka1 / $angka2;
-}
+			switch($operator){
+				case "tambah":
+					$hasil = $angka1 + $angka2;
+					break;
+				case "kurang":
+					$hasil = $angka1 - $angka2;
+					break;
+				case "kali":
+					$hasil = $angka1 * $angka2;
+					break;
+				case "bagi":
+					$hasil = $angka1 / $angka2;
+					break;
+			}
 
-// tampilkan hasil
-echo "Hasil perhitungan: $angka1 $operator $angka2 = $hasil";
-?>
+			echo "<br>Hasil perhitungan: ".$hasil;
+		}
+	?>
+</body>
+</html>
